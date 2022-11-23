78c8763b01a1a8f03e5090205900e2d6c2e651f5
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-11-23 13:45:48 +0700

    reinit backend

--- multer-mern/back-end/server.js
+++ multer-mern/back-end/server.js
@@ -1,17 +1,13 @@
 const express = require("express");
-const path = require("path");
 const multer = require("multer");
-const app = express();
-require("dotenv").config();
-const PORT = process.env.PORT || 3500;
 
-// middlewares
-app.use(express.static(path.join(__dirname, "uploads")));
+const path = require("path");
+const app = express();
 
 // multer config
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads");
+    cb(null, path.join(__dirname, "uploads"));
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
@@ -20,18 +16,16 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+// middlewares
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "uploads")));
+
 // routes
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+app.post("/upload", upload.single("photo"), (req, res) => {
+  // username, photo
+  res.json({ message: `user ${username} created!` });
 });
 
-app.post("/upload", upload.single("image"), (req, res) => {
-  let response = "<p>Upload Success</p>";
-  response += "<a href='/'>Home</a><br />";
-  response += `<img src='/${req.file.filename}' alt='${req.file.filename}' />`;
-  res.send(response);
-});
-
-app.listen(PORT, () => {
-  console.log("server running on port " + PORT);
+app.listen(3500, () => {
+  console.log("server running on port 3500");
 });
